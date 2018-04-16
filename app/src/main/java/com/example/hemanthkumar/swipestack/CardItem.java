a14199b364f7701d383d4e098710ad726f66d83b
hemanthb93 <hemanthkumar@mirafra.com>
2018-04-16 14:59:54 +0530

    here I am adding Swipestack code

---
+++ app/src/main/java/com/example/hemanthkumar/swipestack/CardItem.java
@@ -0,0 +1,8 @@
+package com.example.hemanthkumar.swipestack;
+
+/**
+ * Created by hemanthkumar on 16/1/18.
+ */
+
+public class CardItem {
+}
---
+++ app/src/main/java/com/example/hemanthkumar/swipestack/CardsAdapter.java
@@ -0,0 +1,47 @@
+package com.example.hemanthkumar.swipestack;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by hemanthkumar on 16/1/18.
+ */
+
+public class CardsAdapter extends BaseAdapter {
+
+    private Activity activity;
+    private List<CardItem> data;
+
+    public CardsAdapter(Activity activity, List<CardItem> data) {
+        this.data = data;
+        this.activity = activity;
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+
+        }
+
